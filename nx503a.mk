8ebf2dff26c1f3a3d12531f60129ea32b56212d3
Amy Maloche <amaloche@codeaurora.org>
2013-09-19 12:45:43 -0700

    Add Key Layout file for Synaptics Touch Controller
    Add support for virtual key functionality.
    
    Change-Id: Ifed7813c71658d1111f223f46ac8aa700f995108

--- nx503a.mk
+++ nx503a.mk
@@ -327,7 +327,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
     $(LOCAL_PATH)/usr/keylayout/Generic.kl:system/usr/keylayout/Generic.kl \
     $(LOCAL_PATH)/usr/keylayout/atmel_mxt_ts.kl:system/usr/keylayout/atmel_mxt_ts.kl \
-    $(LOCAL_PATH)/usr/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl 
+    $(LOCAL_PATH)/usr/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/usr/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
 
 # External key layouts
 PRODUCT_COPY_FILES += \
