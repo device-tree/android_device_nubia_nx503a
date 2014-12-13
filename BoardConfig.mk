c4da25b1d79093b9d8e95c382514752c917f587a
David Ng <dave@codeaurora.org>
2014-12-12 17:17:27 -0800

    Set bootdevice flag
    Add bootdevice parameter to kernel command line to indicate the
    boot storage device (msm_sdcc.1).
    
    Change-Id: Ie026eaf7724f22c89c1be6ef5a7d8378f458284a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,7 +85,7 @@ TARGET_KRAIT_BIONIC_PLDSIZE := 64
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
