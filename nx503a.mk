f2e6491b919e2ec52d221a5f92a8a3310eb5695c
BambooIV <ningdisheng@gmail.com>
2016-03-25 13:49:55 +0800

    property: Merge H213

--- nx503a.mk
+++ nx503a.mk
@@ -361,15 +361,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.no_wait_for_card=1
 
+# Z5S
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=72 \
-    ro.hwui.layer_cache_size=48 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.path_cache_size=32 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024
+    ro.bluetooth.ftm_enabled=true \
+    persist.factory.keypad_type=standard_with_camera_with_focus \
+    ro.nubia.haptics.default=30,22,30,22,22,30,38,22,30,22,30,30 \
+    ro.com.google.clientidbase=android-zte \
+    ro.com.google.clientidbase.ms=android-zte \
+    ro.com.google.clientidbase.am=android-zte \
+    ro.com.google.clientidbase.gmm=android-zte \
+    ro.com.google.clientidbase.yt=android-zte \
+    audio.effect.name=dolby \
+    persist.sys.singlehand=1
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=none
