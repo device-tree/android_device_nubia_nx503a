04e9bb32bb5be652b45f121a1cefed704f68cdf4
BambooIV <ningdisheng@gmail.com>
2016-04-01 22:42:44 +0800

    Revert "perf: Change ro property value"
    - use libqc-opt.so from H213
    
    This reverts commit 2f9b4c5c072808631a2177224be4a96c5ac03d2d.

--- nx503a.mk
+++ nx503a.mk
@@ -343,7 +343,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/etc/telephony/NX513J_Z78-1.xml:system/etc/telephony/NX513J_Z78-1.xml
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=libqti-perfd-client.so \
+    ro.vendor.extension_library=/vendor/lib/libqc-opt.so \
     persist.radio.apm_sim_not_pwdn=1
 
 # CDMA
