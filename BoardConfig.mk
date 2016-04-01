46cca221502e9618b644fba179a7986e736d65f3
BambooIV <ningdisheng@gmail.com>
2016-04-01 16:11:40 +0800

    nx503a: Remove DNO_SECURE_DISCARD
    - we are using prebuilt ril now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,7 +57,6 @@ TARGET_CPU_VARIANT := krait
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
-COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
